05a7ba4810c179a736b3cc8108c1e4b6bf307101
paku <paku@skyizwhite.dev>
2022-12-17 15:53:47 +0900

    Use uninterned symbols instead

--- jingle-app.asd
+++ jingle-app.asd
@@ -4,6 +4,6 @@
   :version "0.0.1"
   :description "Playground app using jingle, the web framework for Common Lisp"
   :pathname "src"
-  :depends-on (:jingle-app/main))
+  :depends-on ("jingle-app/main"))
 
-(register-system-packages "lack-middleware-accesslog" '(:lack.middleware.accesslog))
+(register-system-packages "lack-middleware-accesslog" '(#:lack.middleware.accesslog))
--- src/main.lisp
+++ src/main.lisp
@@ -1,33 +1,27 @@
-(defpackage :jingle-app/main
-  (:use :cl)
-  (:import-from :jingle
-                :make-app
-                :install-middleware
-                :with-request-params
-                :with-json-response
-                :set-response-status
-                :route)
-  (:import-from :dexador)
-  (:import-from :jonathan))
-(in-package :jingle-app/main)
+(defpackage #:jingle-app/main
+  (:use #:cl)
+  (:import-from #:jingle)
+  (:import-from #:dexador)
+  (:import-from #:jonathan))
+(in-package #:jingle-app/main)
 
 (defparameter *catfact-endpoint* "https://catfact.ninja/fact")
-(defparameter *app* (make-app))
+(defparameter *app* (jingle:make-app))
 
 (defun hello (params)
-  (with-request-params ((name "name" "World")) params
-    (with-json-response
-      `(:|text| ,(format nil "Hello, ~a" name)))))
+  (jingle:with-request-params ((name "name" "World")) params
+    (jingle:with-json-response
+        (list :|text| (format nil "Hello, ~a" name)))))
 
 (defun collect-cat-facts (&key counts)
-  `(:|facts| ,(loop :repeat counts
-                    :collect (getf (jojo:parse (dex:get *catfact-endpoint*))
-                                   :|fact|))))
+  (list :|facts| (loop :repeat counts
+                       :collect (getf (jojo:parse (dex:get *catfact-endpoint*))
+                                      :|fact|))))
 
 (defun cat (params)
   (declare (ignore params))
-  (with-json-response
+  (jingle:with-json-response
     (collect-cat-facts :counts 5)))
 
-(setf (route *app* "/hello") #'hello)
-(setf (route *app* "/cat") #'cat)
+(setf (jingle:route *app* "/hello") #'hello)
+(setf (jingle:route *app* "/cat") #'cat)
